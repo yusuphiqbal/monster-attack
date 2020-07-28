59c074bf27ab66929271d1915b47b48467c70c2e
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-28 11:24:39 +0300

    Added logic for displaying logs in heal and specialAttack functions

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function () {
       let damage = this.calculateDamage(3, 10);
@@ -28,8 +29,14 @@ new Vue({
       this.monsterAttacks();
     },
     specialAttack: function () {
-      this.monsterHealth -= this.calculateDamage(10, 20);
-
+      let damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      
+      this.turns.unshift({
+        isPlayer: true,
+        text: `Player hits Monster hard for ${damage}`
+      });
+      
       if (this.checkWin()) {
         return;
       }
@@ -42,6 +49,10 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
       this.monsterAttacks();
     },
     giveUp: function () {
