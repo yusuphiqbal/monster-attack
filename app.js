8c561649db750ebbcb5787d8d7f4ad0b81e7d5dd
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-28 11:15:02 +0300

    Added turns array and logic for displaying logs

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function () {
@@ -12,7 +13,13 @@ new Vue({
       this.monsterHealth = 100;
     },
     attack: function () {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      let damage = th.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      
+      this.turns.unshift({
+        isPlayer: true,
+        text: `Player hits Monster for ${damage}`
+      });
 
       if (this.checkWin()) {
         return;
@@ -41,7 +48,14 @@ new Vue({
       this.gameIsRunning = false;
     },
     monsterAttacks: function () {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      let damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      
+      this.turns.unshift({
+        isPlayer: false,
+        text: `Monster hits Player for ${damage}`
+      });
+      
       this.checkWin();
     },
     calculateDamage: function (min, max) {
