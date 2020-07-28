d8e6a571ad13bf134a6abd9600c5d250173e9503
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-28 11:07:41 +0300

    Formatted code

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ new Vue({
     gameIsRunning: false
   },
   methods: {
-    startGame: function() {
+    startGame: function () {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
-    attack: function() {
+    attack: function () {
       this.monsterHealth -= this.calculateDamage(3, 10);
 
       if (this.checkWin()) {
@@ -20,34 +20,34 @@ new Vue({
 
       this.monsterAttacks();
     },
-    specialAttack: function() {
+    specialAttack: function () {
       this.monsterHealth -= this.calculateDamage(10, 20);
-      
+
       if (this.checkWin()) {
         return;
       }
 
       this.monsterAttacks();
     },
-    heal: function() {
-      if  (this.playerHealth <= 90) {
+    heal: function () {
+      if (this.playerHealth <= 90) {
         this.playerHealth += 10;
       } else {
         this.playerHealth = 100;
       }
       this.monsterAttacks();
     },
-    giveUp: function() {
+    giveUp: function () {
       this.gameIsRunning = false;
     },
-    monsterAttacks: function() {
+    monsterAttacks: function () {
       this.playerHealth -= this.calculateDamage(5, 12);
       this.checkWin();
     },
-    calculateDamage: function(min, max) {
+    calculateDamage: function (min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
-    checkWin: function() {
+    checkWin: function () {
       if (this.monsterHealth <= 0) {
         if (confirm('You won. New game?')) {
           this.startGame();
